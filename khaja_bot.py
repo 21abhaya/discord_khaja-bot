7d69c219e1d69c665efe983b8ed4ca661914f11c
abhaya <gautamabhaya98@gmail.com>
2025-12-20 22:19:49 +0545

    init bot test

--- khaja_bot.py
+++ khaja_bot.py
@@ -1,3 +1,26 @@
-import discord
 
-print(discord.__version__)
+import os
+import discord 
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+token = os.getenv('DISCORD_TOKEN')
+intents = discord.Intents.default()
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f"We have logged in as {client.user}")
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('$hello'):
+        await message.channel.send("Hello!")
+
+client.run(token)
