4b8bb5b341154fb251ef1f4cd6e28034eda6102f
abhaya <gautamabhaya98@gmail.com>
2025-12-21 17:14:17 +0545

    wip: khaja bot first draft

--- khaja_bot.py
+++ khaja_bot.py
@@ -2,25 +2,22 @@
 import os
 import discord 
 
+from discord.ext import commands
+
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 token = os.getenv('DISCORD_TOKEN')
+
 intents = discord.Intents.default()
+intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-@client.event
-async def on_ready():
-    print(f"We have logged in as {client.user}")
-    
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    
-    if message.content.startswith('$hello'):
-        await message.channel.send("Hello!")
+bot = commands.Bot(command_prefix='/', intents=intents)
 
-client.run(token)
+@bot.command()
+async def foo(ctx):
+    await print("testing bot command!")
